4e122fe99696f48ba16d4ba750e3de755223fe0e
Luis Angel <luis.rodriguezreiners@gmail.com>
2023-10-02 22:57:59 +0200

    progress bar, max amount of runs as parameter

--- asp_optimization_orders/main.py
+++ asp_optimization_orders/main.py
@@ -9,13 +9,8 @@ import random
 from statistics import mean
 from tqdm import tqdm
 
-runs = 10
-
-# Options
-options = "hf:"
-
- # Long options
-long_options = ["Help", "Facts"]
+max_warehouses = 10
+max_products = 10
 
 def main():
 
@@ -23,18 +18,21 @@ def main():
         description='''Cart Optimization. ''',
         epilog="""Hope you get the best fit!!."""
     )
-    parser.add_argument('--test', action='store_false', help='Run with random inputs', required=True)
+    parser.add_argument('--test', action='store_true', help='Run with random inputs', required=True)
+    parser.add_argument('-r', nargs=1, help='Max amount of runs', type=int, required=True)
+    args=parser.parse_args()
+    max_runs=args.r[0]
 
     ENCODING = read_file("./encoding.asp")
-    control = Control(logger=logger)
     generator = Generator()
     run_times = []
-    for _ in tqdm(range(runs), desc="Testing..."):
-        t_warehouses = random.randint(1, 10)
-        t_products = random.randint(1, 10)
+    for _ in tqdm(range(max_runs), desc="Testing..."):
+        t_warehouses = random.randint(1, max_warehouses)
+        t_products = random.randint(1, max_products)
         facts = generator.generate_random_input(t_warehouses, t_products)
     
         try:
+            control = Control(logger=logger)
             start_time = time.time()
             control.add(f"{facts}\n{ENCODING}")
             control.ground([("base", [])], context=Context())
@@ -48,10 +46,14 @@ def main():
             # print(generator.get_shippings_costs())
             # print(generator.calculate_cost(res))
 
-        except Exception:
+        except Exception as e:
             pass
 
-    print(f"Mean execution time: {mean(run_times)}")
+    if len(run_times) > 0:
+        print(f"Solutions found: {len(run_times)}")
+        print(f"Mean execution time: {mean(run_times)}")
+    else:
+        print("No solutions found")
 
 if __name__ == '__main__':
     main()
