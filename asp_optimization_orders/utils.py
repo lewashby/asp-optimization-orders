be61fb65c6a4e1d3833ea523337025d04d46b6f7
Luis Angel <luis.rodriguezreiners@gmail.com>
2023-10-03 23:52:35 +0200

    calculate cost on utils

--- asp_optimization_orders/utils.py
+++ asp_optimization_orders/utils.py
@@ -1,4 +1,4 @@
-
+from functools import reduce
 
 def read_file(dir):
     file = open(dir, "r")
@@ -7,11 +7,12 @@ def read_file(dir):
     return data
 
 def create_json_object(result):
-    splitted = result.split(',')
+    values = result[result.find("(")+1:result.find(")")]
+    splitted = values.split(',')
     return {
-        "product": int(splitted[0][-1]), 
-        "warehouse": int(splitted[1][0]), 
-        "quantity": int(splitted[2][0])
+        "warehouse": int(splitted[1]), 
+        "product": int(splitted[0]), 
+        "quantity": int(splitted[2])
     }
 
 def parse_response(result):
@@ -20,3 +21,16 @@ def parse_response(result):
 
 def logger(code, msg):
     return
+
+def calculate_cost(result: list, p_prices: list, w_shipping_cost: list, w_threshold: list):
+    total = 0
+
+    for i, cost in enumerate(w_shipping_cost):
+        index = i+1
+        warehouse_allocations = list(filter(lambda x: x['warehouse'] == index, result))
+        if (len(warehouse_allocations) > 0):
+            spent = reduce(lambda sum, x: sum + x['quantity']*p_prices[x['product']-1], warehouse_allocations, 0)
+            if spent > w_threshold[i]:
+                cost = 0
+            total+=cost
+    return total
