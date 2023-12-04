eedea9def25562bdeb9b9ea51f70422e3cbebdf1
Luis Angel <luis.rodriguezreiners@gmail.com>
2023-12-04 12:35:55 +0100

    solver as parameter

--- asp_optimization_orders/miniznc.py
+++ asp_optimization_orders/miniznc.py
@@ -3,9 +3,9 @@ from utils import calculate_cost
 
 class MinizincSolver:
 
-    def __init__(self, encoding_path):
+    def __init__(self, encoding_path, solver="gecode"):
         model = Model(encoding_path)
-        gecode = Solver.lookup("gecode")
+        gecode = Solver.lookup(solver)
         self.model = Instance(gecode, model)
 
     def solve(self, parameters: dict):
@@ -15,11 +15,13 @@ class MinizincSolver:
     
     @staticmethod
     def parse_solution(result):
-        solution = result.solution
-        res = []
-        for i, p in enumerate(solution.select):
-            res += [{"warehouse": j+1, "product": i+1, "quantity": w } for j, w in enumerate(p) if w != 0]
-        return {"objective": result.objective, "solution": res}
+        if (result.solution is not None):
+            solution = result.solution
+            res = []
+            for i, p in enumerate(solution.select):
+                res += [{"warehouse": j+1, "product": i+1, "quantity": w } for j, w in enumerate(p) if w != 0]
+            return {"objective": result.objective, "solution": res}
+        return None
     
     @staticmethod
     def calculate_cost(result: list, p_prices: list, w_shipping_cost: list, w_threshold: list):
