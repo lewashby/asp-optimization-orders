78cc8f2d6e835f59121c9f614446f15262233089
Luis Angel <luis.rodriguezreiners@gmail.com>
2024-01-29 13:18:04 +0100

    solver with timeout

--- asp_optimization_orders/solver.py
+++ asp_optimization_orders/solver.py
@@ -11,14 +11,28 @@ class Solver:
     def __init__(self, encoding_path):
         self.__encoding = read_file(encoding_path)
     
+    
 
-    def solve(self, facts, arguments=["--opt-strategy=usc,k,0,5" ,"--opt-usc-shrink=rgs"]):
+    def solve(self, facts, arguments=["--opt-strategy=usc,k,0,5" ,"--opt-usc-shrink=rgs"], timeout=2):
+
+        results: list = []
+        handle = None
+        def on_model(m):
+            results.append(Model.of_atoms(m.symbols(shown=True)))
+        
         control = Control(arguments, logger=logger)
         control.add(f"{facts}\n{self.__encoding}")
         control.ground([("base", [])], context=Context())
-        model = Model.of_control(control)
-        result = model.as_facts.split('\n')
-        return parse_response(result)
+
+        with control.solve(on_model=on_model, async_=True) as handle:
+            handle.wait(timeout)
+            handle.cancel()
+            handle = handle.get()
+            
+        model = results[0] if len(results) > 0 else Model.empty()
+        result = model.as_facts.split('\n') if len(model) > 0 else []
+        
+        return parse_response(result), handle.interrupted, handle.satisfiable
     
     @staticmethod
     def calculate_cost(result: list, p_prices: list, w_shipping_cost: list, w_threshold: list):
